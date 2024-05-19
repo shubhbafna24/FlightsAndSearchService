72720f361080727cd7d5d50675fb3f8a6e985da6
shubhbafna24 <shubhbafna24@gmail.com>
2024-05-20 05:03:40 +0530

    Setup basic dotenv

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express=require("express");
+
+const{ PORT }=require('./config/serverConfig');
+
+const setupAndStartServer= async()=>{
+    //create the express object
+    const app=express();
+    
+    app.listen(3000,()=>{
+        console.log(`Server started at ${PORT}`);
+        //console.log(process.env);
+    })
+}
+
+setupAndStartServer();

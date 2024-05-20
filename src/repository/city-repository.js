d8aa39cea11d5fb15968ba3448e1076a4406f31e
shubhbafna24 <shubhbafna24@gmail.com>
2024-05-21 03:42:45 +0530

    Added filter to get all cities

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const { City } = require('../models/index');
 
 class CityRepository{
--- src/services/city-service.js
+++ src/services/city-service.js
@@ -1,3 +1,5 @@
+
+
 const { CityRepository } = require('../repository/index');
 
 class CityService {
